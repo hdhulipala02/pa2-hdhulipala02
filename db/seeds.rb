ed432592d5850fec846c2494e46ac8b906d9a634
“hdhulipala02” <hdhulipala02@tamu.edu>
2022-12-27 18:50:00 +0800

    added favorite movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
     {:title => 'Harry Potter', :rating => 'PG',
     :release_date => '10-May-2001'},
 ]
