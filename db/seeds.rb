51cd6ec017fec1de19c15839944b67194224550f
“hdhulipala02” <hdhulipala02@tamu.edu>
2022-12-27 18:48:42 +0800

    added favorite movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ more_movies = [
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
     {:title => 'Harry Potter', :rating => 'PG',
-    :release_date => '10-May-2001'}  
+    :release_date => '10-May-2001'},
 ]
 
 more_movies.each do |movie|
